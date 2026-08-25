3ff785f6939e316cc5840a4625fcfa8560be93e0
大鱼 <wjh84329@gmail.com>
2026-08-25 18:52:03 +0800

    style: rebalance legacy detail columns

--- src/views/partmoduleClone.vue
+++ src/views/partmoduleClone.vue
@@ -366,10 +366,10 @@
                   <table style="width: 100%; min-width: 0; table-layout: fixed;">
                     <colgroup>
                       <col style="width: 130px;" />
-                      <col style="width: 240px;" />
-                      <col style="width: 100px;" />
                       <col style="width: 180px;" />
-                      <col style="width: 70px;" />
+                      <col style="width: 80px;" />
+                      <col style="width: 180px;" />
+                      <col style="width: 120px;" />
                       <col style="width: 120px;" />
                       <col style="width: 80px;" />
                     </colgroup>
