4ac0e0c082951b4eaf037117ad9577890c679633
大鱼 <wjh84329@gmail.com>
2026-08-25 18:47:29 +0800

    fix: force legacy detail table to use content width

--- src/views/partmoduleEdit.vue
+++ src/views/partmoduleEdit.vue
@@ -367,8 +367,8 @@
             <dt>附加详情：</dt>
             <dd>
               <span class="inputbox">
-                <div class="tablebox legacy-additional-detail-table">
-                  <table style="width: 100%; min-width: 0;">
+                <div class="tablebox legacy-additional-detail-table" style="width: 100%; max-width: 1000px;">
+                  <table style="width: 100%; min-width: 0; table-layout: fixed;">
                     <colgroup>
                       <col style="width: 130px;" />
                       <col style="width: 240px;" />
